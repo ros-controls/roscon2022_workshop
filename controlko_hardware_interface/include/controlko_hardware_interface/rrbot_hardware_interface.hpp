26e2b1e45ca1f68c7ed6dd9b7dc211b751061962
Denis Štogl <denis@stoglrobotics.de>
2022-10-16 17:30:53 +0200

    Task 7 implemenation of RRBot hardware interface.

--- controlko_hardware_interface/include/controlko_hardware_interface/rrbot_hardware_interface.hpp
+++ controlko_hardware_interface/include/controlko_hardware_interface/rrbot_hardware_interface.hpp
@@ -1,5 +1,4 @@
-// Copyright (c) 2022, ROSCon2022 ros2_control Workshop
-// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,13 +15,16 @@
 #ifndef CONTROLKO_HARDWARE_INTERFACE__RRBOT_HARDWARE_INTERFACE_HPP_
 #define CONTROLKO_HARDWARE_INTERFACE__RRBOT_HARDWARE_INTERFACE_HPP_
 
+#include <memory>
 #include <string>
 #include <vector>
 
+#include "dr_denis_rrbot_comms.hpp"
+
 #include "controlko_hardware_interface/visibility_control.h"
-#include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
 #include "rclcpp/macros.hpp"
 #include "rclcpp_lifecycle/state.hpp"
@@ -47,11 +49,25 @@ public:
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
   TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::return_type prepare_command_mode_switch(
+    const std::vector<std::string> & start_interfaces,
+    const std::vector<std::string> & stop_interfaces) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::return_type perform_command_mode_switch(
+    const std::vector<std::string> & start_interfaces,
+    const std::vector<std::string> & stop_interfaces) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
   TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
   hardware_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
+  hardware_interface::CallbackReturn on_shutdown(
     const rclcpp_lifecycle::State & previous_state) override;
 
   TEMPLATES__ROS2_CONTROL__VISIBILITY_PUBLIC
@@ -63,8 +79,23 @@ public:
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
 private:
-  std::vector<double> hw_commands_;
-  std::vector<double> hw_states_;
+  std::vector<double> joint_pos_commands_;
+  std::vector<double> joint_vel_commands_;
+  std::vector<double> joint_pos_states_;
+  std::vector<double> joint_vel_states_;
+  std::vector<double> joint_acc_states_;
+
+  std::vector<double> gpio_commands_;
+  std::vector<double> gpio_states_;
+  std::vector<bool> gpio_ins_storage_;
+  std::vector<bool> gpio_outs_storage_;
+  std::vector<bool> gpio_cmds_storage_;
+  std::vector<double> sensor_states_;
+
+  std::vector<std::string> position_command_interface_names_;
+  std::vector<std::string> velocity_command_interface_names_;
+
+  std::unique_ptr<dr_denis_rrbot_comms::DrDenisRRBotComms> rrbot_comms_;
 };
 
 }  // namespace controlko_hardware_interface
--- controlko_hardware_interface/src/rrbot_hardware_interface.cpp
+++ controlko_hardware_interface/src/rrbot_hardware_interface.cpp
@@ -1,5 +1,4 @@
-// Copyright (c) 2022, ROSCon2022 ros2_control Workshop
-// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt) (template)
+// Copyright (c) 2022, Stogl Robotics Consulting UG (haftungsbeschränkt)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <netdb.h>
+#include <chrono>
+#include <cmath>
 #include <limits>
+#include <memory>
 #include <vector>
 
 #include "controlko_hardware_interface/rrbot_hardware_interface.hpp"
@@ -29,9 +32,21 @@ hardware_interface::CallbackReturn RRBotHardwareInterface::on_init(
     return CallbackReturn::ERROR;
   }
 
-  // TODO(anyone): read parameters and initialize the hardware
-  hw_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
-  hw_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_pos_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_vel_commands_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_pos_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_vel_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+  joint_acc_states_.resize(info_.joints.size(), std::numeric_limits<double>::quiet_NaN());
+
+  gpio_commands_.resize(
+    info_.gpios[0].command_interfaces.size(), std::numeric_limits<double>::quiet_NaN());
+  gpio_states_.resize(
+    info_.gpios[0].state_interfaces.size(), std::numeric_limits<double>::quiet_NaN());
+  gpio_ins_storage_.resize(2, false);
+  gpio_outs_storage_.resize(2, false);
+  gpio_cmds_storage_.resize(2, false);
+  sensor_states_.resize(
+    info_.sensors[0].state_interfaces.size(), std::numeric_limits<double>::quiet_NaN());
 
   return CallbackReturn::SUCCESS;
 }
@@ -39,7 +54,7 @@ hardware_interface::CallbackReturn RRBotHardwareInterface::on_init(
 hardware_interface::CallbackReturn RRBotHardwareInterface::on_configure(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  // TODO(anyone): prepare the robot to be ready for read calls and write calls of some interfaces
+  rrbot_comms_ = std::make_unique<dr_denis_rrbot_comms::DrDenisRRBotComms>(info_.joints.size());
 
   return CallbackReturn::SUCCESS;
 }
@@ -47,31 +62,131 @@ hardware_interface::CallbackReturn RRBotHardwareInterface::on_configure(
 std::vector<hardware_interface::StateInterface> RRBotHardwareInterface::export_state_interfaces()
 {
   std::vector<hardware_interface::StateInterface> state_interfaces;
-  for (size_t i = 0; i < info_.joints.size(); ++i) {
+  state_interfaces.reserve(info_.joints.size() * 3);
+  for (size_t i = 0; i < info_.joints.size(); ++i)
+  {
     state_interfaces.emplace_back(hardware_interface::StateInterface(
-      // TODO(anyone): insert correct interfaces
-      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_[i]));
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joint_pos_states_[i]));
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &joint_vel_states_[i]));
+    state_interfaces.emplace_back(hardware_interface::StateInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_EFFORT, &joint_acc_states_[i]));
+  }
+
+  size_t i = 0;
+  for (const auto & state_itf : info_.gpios[0].state_interfaces)
+  {
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(info_.gpios[0].name, state_itf.name, &sensor_states_[i]));
+    ++i;
+  }
+
+  i = 0;
+  for (const auto & state_itf : info_.sensors[0].state_interfaces)
+  {
+    state_interfaces.emplace_back(
+      hardware_interface::StateInterface(info_.sensors[0].name, state_itf.name, &gpio_states_[i]));
+    ++i;
   }
 
   return state_interfaces;
 }
 
-std::vector<hardware_interface::CommandInterface> RRBotHardwareInterface::export_command_interfaces()
+std::vector<hardware_interface::CommandInterface>
+RRBotHardwareInterface::export_command_interfaces()
 {
   std::vector<hardware_interface::CommandInterface> command_interfaces;
-  for (size_t i = 0; i < info_.joints.size(); ++i) {
+  command_interfaces.reserve(info_.joints.size() * 2);
+  position_command_interface_names_.reserve(info_.joints.size());
+  velocity_command_interface_names_.reserve(info_.joints.size());
+  for (size_t i = 0; i < info_.joints.size(); ++i)
+  {
     command_interfaces.emplace_back(hardware_interface::CommandInterface(
-      // TODO(anyone): insert correct interfaces
-      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
+      info_.joints[i].name, hardware_interface::HW_IF_POSITION, &joint_pos_commands_[i]));
+    position_command_interface_names_.push_back(command_interfaces.back().get_name());
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &joint_vel_commands_[i]));
+    velocity_command_interface_names_.push_back(command_interfaces.back().get_name());
+  }
+
+  size_t i = 0;
+  for (const auto & command_itf : info_.sensors[0].command_interfaces)
+  {
+    command_interfaces.emplace_back(hardware_interface::CommandInterface(
+      info_.sensors[0].name, command_itf.name, &gpio_states_[i]));
+    ++i;
   }
 
   return command_interfaces;
 }
 
+hardware_interface::return_type RRBotHardwareInterface::prepare_command_mode_switch(
+  const std::vector<std::string> & start_interfaces,
+  const std::vector<std::string> & stop_interfaces)
+{
+  bool starting_at_least_one_pos_itf = false;
+  bool starting_at_least_one_vel_itf = false;
+
+  for (const auto & itf : position_command_interface_names_)
+  {
+    auto find_it = std::find(start_interfaces.begin(), start_interfaces.end(), itf);
+    if (find_it != start_interfaces.end())
+    {
+      starting_at_least_one_pos_itf = true;
+    }
+  }
+  for (const auto & itf : velocity_command_interface_names_)
+  {
+    auto find_it = std::find(start_interfaces.begin(), start_interfaces.end(), itf);
+    if (find_it != start_interfaces.end())
+    {
+      starting_at_least_one_vel_itf = true;
+    }
+  }
+
+  bool stopping_all_pos_itfs = true;
+  bool stopping_all_vel_itfs = true;
+
+  for (const auto & itf : position_command_interface_names_)
+  {
+    auto find_it = std::find(stop_interfaces.begin(), stop_interfaces.end(), itf);
+    if (find_it == stop_interfaces.end())
+    {
+      stopping_all_pos_itfs = false;
+    }
+  }
+  for (const auto & itf : velocity_command_interface_names_)
+  {
+    auto find_it = std::find(stop_interfaces.begin(), stop_interfaces.end(), itf);
+    if (find_it == stop_interfaces.end())
+    {
+      stopping_all_vel_itfs = false;
+    }
+  }
+
+  auto current_control_mode = rrbot_comms_->get_control_mode();
+
+  return hardware_interface::return_type::OK;
+}
+
+hardware_interface::return_type RRBotHardwareInterface::perform_command_mode_switch(
+  const std::vector<std::string> & /*start_interfaces*/,
+  const std::vector<std::string> & /*stop_interfaces*/)
+{
+  // switch between position and velocity modes
+  return hardware_interface::return_type::OK;
+}
+
 hardware_interface::CallbackReturn RRBotHardwareInterface::on_activate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  // TODO(anyone): prepare the robot to receive commands
+  rrbot_comms_->init();
+
+  // set some default values for joints
+  rrbot_comms_->read_joint_states(joint_pos_states_, joint_vel_states_, joint_acc_states_);
+
+  joint_pos_commands_ = joint_pos_states_;
+  joint_vel_commands_ = joint_vel_states_;
 
   return CallbackReturn::SUCCESS;
 }
@@ -79,15 +194,29 @@ hardware_interface::CallbackReturn RRBotHardwareInterface::on_activate(
 hardware_interface::CallbackReturn RRBotHardwareInterface::on_deactivate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
-  // TODO(anyone): prepare the robot to stop receiving commands
+  rrbot_comms_->stop();
+  return CallbackReturn::SUCCESS;
+}
 
+hardware_interface::CallbackReturn RRBotHardwareInterface::on_shutdown(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  rrbot_comms_.release();
   return CallbackReturn::SUCCESS;
 }
 
 hardware_interface::return_type RRBotHardwareInterface::read(
   const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  // TODO(anyone): read robot states
+  rrbot_comms_->read_joint_states(joint_pos_states_, joint_vel_states_, joint_acc_states_);
+  rrbot_comms_->read_sensor_values(sensor_states_);
+
+  rrbot_comms_->read_gpio(gpio_ins_storage_, gpio_outs_storage_);
+  for (size_t i = 0; i < 2; ++i)
+  {
+    gpio_states_[i] = static_cast<double>(gpio_ins_storage_[i]);
+    gpio_states_[2 + i] = static_cast<double>(gpio_outs_storage_[i]);
+  }
 
   return hardware_interface::return_type::OK;
 }
@@ -95,7 +224,13 @@ hardware_interface::return_type RRBotHardwareInterface::read(
 hardware_interface::return_type RRBotHardwareInterface::write(
   const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  // TODO(anyone): write robot's commands'
+  rrbot_comms_->write_joint_commands(joint_pos_commands_, joint_vel_commands_);
+
+  for (size_t i = 0; i < info_.gpios[0].command_interfaces.size(); ++i)
+  {
+    gpio_cmds_storage_[i] = static_cast<bool>(gpio_commands_[i]);
+  }
+  rrbot_comms_->write_gpios(gpio_cmds_storage_);
 
   return hardware_interface::return_type::OK;
 }
